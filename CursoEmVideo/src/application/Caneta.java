ce81d907f3518ff80b3e1970cc0b74015b5f0e82
danilosheen <c.danilo.f.silva@gmail.com>
2020-02-25 13:57:51 -0300

    add files

---
+++ CursoEmVideo/src/application/Caneta.java
@@ -0,0 +1,37 @@
+package application;
+
+public class Caneta {
+	
+	String modelo;
+	String cor;
+	float ponta;
+	int carga;
+	boolean tampada;
+	
+	void status() {
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("Cor: " + this.cor);
+		System.out.println("Ponta: " + this.ponta);
+		System.out.println("Qual a carga? " + this.carga);
+		System.out.println("Está tampada? " + this.tampada);
+	}
+	
+	void rabiscar() {
+		if (this.tampada == true){
+			System.out.println("A caneta está tampada, não posso escrever");
+		}
+		else {
+			System.out.println("A caneta está destampada, estou escrevendo");
+			
+		}
+	}
+	
+	void tampar() {
+		this.tampada = true;
+	}
+	
+	void destampar() {
+		this.tampada = false;
+	}
+
+}
